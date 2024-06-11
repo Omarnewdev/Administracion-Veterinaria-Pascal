134c22c3b3981c622cf6aa2fd3efa499fc962b68
Erlan Omar <erlanomararcevasquez@gmail.com>
2024-06-11 14:14:07 -0300

    Falta terminar la Unidad Animales, personas  controlador

--- uanimales.pas
+++ uanimales.pas
@@ -5,9 +5,303 @@ unit UAnimales;
 interface
 
 uses
-  Classes, SysUtils;
+  UArchivos,Classes, SysUtils;
+
+type
+
+  { TEspecie }
+
+  TEspecie= class(TArchivable)
+    strict private
+      type
+          TEspecieFileRecord = packed record
+            FatrEspecie:String[20];
+            FatrRaza:String[20];
+          end;
+    private
+      type
+          TArchivoEspecie = file of TEspecieFileRecord;
+    private
+      atrEspecie: String;
+      atrRaza: String;
+    public
+      constructor create(pEspecie,pRaza:String); overload;
+      destructor Destroy; override;
+      function getEspecie:String;
+      procedure setEspecie(pEspecie:String);
+      function getRaza:String;
+      procedure setRaza(pRaza:String);
+      function guardarEnArchivo(direccionCompleta:string):boolean; override;
+      function clonar:TArchivable; override;
+      class function recuperarDeArchivo(direccionCompleta,idBusqueda:string):TArchivable; override; //Se iniciara con inizialition
+      class function recuperarLista(direccionCompleta:String):TListaArchivable; override; //Se iniciara con inizialitation
+      property Especie:String read getEspecie write setEspecie;
+      property Raza:String read getRaza write setRaza;
+  end;
+
+
+  { TAnimal }
+
+  TAnimal = class(TArchivable)
+    public
+      type
+          TGenero = (ANIMAL_MACHO,ANIMAL_HEMBRA,PERSONA_MASCULINO,PERSONA_FEMENINO);
+          TGestacion = (OVIPARO,VIVIPARO);
+    protected
+      atrNombre:String;
+      atrGenero:String;
+      atrGeneroEspecifico:TGenero;
+      atrEdad: byte;
+      atrGestacion: String;
+      atrGestacionEsp:Tgestacion;
+    public
+
+
+      function getFicha:string; virtual; abstract;
+
+      constructor Create(pNOMBRE:String; pGenero:TGenero; pEdad:byte; pGestacion:TGestacion); overload;
+      destructor Destroy; override;
+      function getEdad: byte; virtual;
+      function getGeneroEsp: TGenero; virtual;
+      function getGeneroStr: String; virtual;
+      function getGestacionEsp: Tgestacion; virtual;
+      function getGestacionStr: String; virtual;
+      function getNombre: String; virtual;
+      procedure setEdad(AValue: byte); virtual;
+      procedure setGenero(AValue: TGenero); virtual;
+      procedure setGestacion(AValue: Tgestacion); virtual;
+      procedure setNombre(AValue: String); virtual;
+
+      property Nombre:String read getNombre write setNombre;
+      property Genero:TGenero read getGeneroEsp write setGenero;
+      property GeneroStr:String read getGeneroStr;
+      property Edad: byte read getEdad write setEdad;
+      property Gestacion: Tgestacion read getGestacionEsp write setGestacion;
+      property GestacionStr: String read getGestacionStr;
+  end;
+
+  TMascota = class(TAnimal)
+    private
+      atrID: String;
+      atrEspecie: String;
+
+  end;
+
+
 
 implementation
+
+{ TEspecie }
+
+constructor TEspecie.create(pEspecie, pRaza: String);
+begin
+  self.atrEspecie:=pEspecie;
+  self.atrRaza:=pRaza;
+end;
+
+destructor TEspecie.Destroy;
+begin
+  inherited Destroy;
+end;
+
+function TEspecie.getEspecie: String;
+begin
+  result:=self.atrEspecie;
+end;
+
+procedure TEspecie.setEspecie(pEspecie: String);
+begin
+  self.atrEspecie:=pEspecie;
+end;
+
+function TEspecie.getRaza: String;
+begin
+  result:=self.atrRaza;
+end;
+
+procedure TEspecie.setRaza(pRaza: String);
+begin
+  self.atrRaza:=pRaza;
+end;
+
+function TEspecie.guardarEnArchivo(direccionCompleta: string): boolean;
+var guardar:TEspecieFileRecord;
+    dbEspecie:TArchivoEspecie;
+
+  //Aqui esta un poco dificil porque solo abrira un archivo al invocar estas funciones
+begin
+  AssignFile(dbEspecie,direccionCompleta);
+  {$I-}// Desactiva la deteccion manual de errores
+  if FileExists(direccionCompleta) then
+     reset(dbEspecie)
+  else
+     rewrite(dbEspecie);
+  {$I+}//Vuelve a activar la deteccion manual de errores
+
+  If IOResult<>0 then begin
+    //Aqui salto algun error
+    result:=false;
+    EXIT;
+  end;
+  guardar.FatrRaza:=self.Raza;
+  guardar.FatrEspecie:=self.Especie;
+
+  seek(dbEspecie,FileSize(dbEspecie));
+  write(dbEspecie,guardar);
+
+  {$I-}
+  Close(dbEspecie);
+  {$I+}
+  if IOResult<>0 then
+     result:=false
+  else
+     result:=true;
+
+end;
+
+function TEspecie.clonar: TArchivable;
+begin
+  result := TEspecie.create(self.Especie,self.Raza);
+
+end;
+
+class function TEspecie.recuperarDeArchivo(direccionCompleta, idBusqueda: string
+  ): TArchivable;
+var dbEspecie:TArchivoEspecie;
+    recoger:TEspecieFileRecord;
+//El id lo tomare como si fuera el espacio del archivo
+{Por ejemplo si el tamaño del archivo es menor al id significa que no existe
+Y retorno NIL}
+begin
+  AssignFile(dbEspecie,direccionCompleta);
+  if FileExists(direccionCompleta) then
+     reset(dbEspecie)
+  else
+     rewrite(dbEspecie);
+  if FileSize(dbEspecie)<=StrtoInt(idBusqueda) then begin
+    result:=NIL;
+    EXIT;
+  end;
+  seek(dbEspecie,StrtoInt(idBusqueda));
+  read(dbEspecie,recoger);
+
+  result:=TEspecie.create(recoger.FatrEspecie,recoger.FatrRaza);
+  CloseFile(dbEspecie);
+end;
+
+class function TEspecie.recuperarLista(direccionCompleta: String): TListaArchivable;
+var dbEspecie:TArchivoEspecie;
+    recoger:TEspecieFileRecord;
+    contenido:TEspecie;
+begin
+  result:= TListaArchivable.create;
+  AssignFile(dbEspecie,direccionCompleta);
+  if FileExists(direccionCompleta) then
+     reset(dbEspecie)
+  else
+     rewrite(dbEspecie);
+
+  seek(dbEspecie,0);
+  while not eof(dbEspecie) do begin
+    read(dbEspecie,recoger);
+    contenido:=TEspecie.create(recoger.FatrEspecie,recoger.FatrRaza);
+    result.agregar(contenido);
+  end;
+
+
+
+  Close(dbEspecie);
+end;
+
+{ TAnimal }
+
+constructor TAnimal.Create(pNOMBRE: String; pGenero: TGenero; pEdad: byte;
+  pGestacion: TGestacion);
+begin
+  self.atrNombre:=pNombre;
+  self.atrGeneroEspecifico:=pGenero;
+  self.atrEdad:=pEdad;
+  self.atrGestacionEsp:=pGestacion;
+  case self.atrGeneroEspecifico of
+       ANIMAL_MACHO: self.atrGenero:='MACHO';
+       ANIMAL_HEMBRA: self.atrGenero:='HEMBRA';
+       PERSONA_MASCULINO: self.atrGenero:='MASCULINO';
+       PERSONA_FEMENINO: self.atrGenero:='FEMENINO';
+  end;
+  case self.atrGestacionEsp of
+       OVIPARO: self.atrGestacion:='OVIPARO';
+       VIVIPARO: self.atrGestacion:='VIVIPARO';
+  end;
+end;
+
+destructor TAnimal.Destroy;
+begin
+  inherited Destroy;
+end;
+
+function TAnimal.getEdad: byte;
+begin
+  result:=self.atrEdad;
+end;
+
+function TAnimal.getGeneroEsp: TGenero;
+begin
+  result:=self.atrGeneroEspecifico;
+end;
+
+function TAnimal.getGeneroStr: String;
+begin
+  result:=self.atrGenero;
+end;
+
+function TAnimal.getGestacionEsp: Tgestacion;
+begin
+  result:=self.atrGestacionEsp;
+end;
+
+function TAnimal.getGestacionStr: String;
+begin
+  result:=self.atrGestacion;
+end;
+
+function TAnimal.getNombre: String;
+begin
+  result:=self.atrNombre;
+end;
+
+procedure TAnimal.setEdad(AValue: byte);
+begin
+  self.atrEdad:=AValue;
+end;
+
+procedure TAnimal.setGenero(AValue: TGenero);
+begin
+  self.atrGeneroEspecifico:=Avalue;
+  case self.atrGeneroEspecifico of
+       ANIMAL_MACHO: self.atrGenero:='MACHO';
+       ANIMAL_HEMBRA: self.atrGenero:='HEMBRA';
+       PERSONA_MASCULINO: self.atrGenero:='MASCULINO';
+       PERSONA_FEMENINO: self.atrGenero:='FEMENINO';
+  end;
+end;
+
+procedure TAnimal.setGestacion(AValue: Tgestacion);
+begin
+  self.atrGestacionEsp:=AValue;
+  case self.atrGestacionEsp of
+       OVIPARO: self.atrGestacion:='OVIPARO';
+       VIVIPARO: self.atrGestacion:='VIVIPARO';
+  end;
+end;
+
+procedure TAnimal.setNombre(AValue: String);
+begin
+  self.atrNombre:=AValue;
+end;
+
+
+
+
 
 end.
 
--- uarchivos.pas
+++ uarchivos.pas
@@ -6,14 +6,15 @@ interface
 uses
   Classes, SysUtils;
 
-
+const
+  directorySeparation = '\';// Cambie a '/' Si quieres usarlo en linux
 type
     TListaArchivable = class;
 
     { TArchivable }
 
     TArchivable = class
-      private
+      protected
             const_NOMBRE_DIRECTORIO_DATABASE: string; static;
             const_NOMBRE_ARCHIVO_ESPECIES: string; static;
             const_NOMBRE_ARCHIVO_MASCOTAS: string; static;
@@ -22,17 +23,36 @@ type
             class function getNAE:String;
             class function getNAM:String;
             class function getNAP:String;
+
       public
 
+        //Es la carpeta donde se encuentran los archivos
         property NOMBRE_DIRECTORIO_DATABASE:string read getNDB;
+        //Es donde se guardan los archivos de la clase ESPECIES
         property NOMBRE_ARCHIVO_ESPECIES:string read getNAE;
+        //Es donde se guardan los archivos de la clase MASCOTAS
         property NOMBRE_ARCHIVO_MASCOTAS:string read getNAM;
+        //Es donde se guardan los archivos de la clase PERSONAS
         property NOMBRE_ARCHIVO_PERSONAS:string read getNAP;
 
+        {guardarEnArchivo: Guarda el objeto actual en un archivo.
+        Cada clase que hereda de TArchivable implementa esta operación a su manera.
+        Se retorna TRUE si se pudo guardar la información, FALSE si no se pudo.
+        El argumento direccionCompleta indica la dirección del archivo en el que se guardará
+        la información, incluyendo el nombre y la extensión del mismo}
         function guardarEnArchivo(direccionCompleta:String):boolean; virtual;abstract;
+        {clonar: Crea una copia limpia del objeto y la retorna en un nuevo objeto.}
         function clonar:TArchivable; virtual; abstract;
-        function recuperarDeArchivo(dirreccionCompleta,idBusqueda:String):TArchivable; virtual; abstract;
-        function recuperarLista(direccionCompleta:string):TListaArchivable; virtual; abstract;
+        {recuperarDeArchivo: Busca en el archivo dado por direccionCompleta (incluye nombre y extensión)
+        el registro identificado con idBusqueda y retorna un objeto con su información.
+        Cada clase implementa esta operación a su manera. Si no se encuentra el registro
+        se retorna NIL.}
+        class function recuperarDeArchivo(dirreccionCompleta,idBusqueda:String):TArchivable; virtual; abstract;
+        {recuperarLista: Busca en la dirección dada (incluye nombre y extensión) todos los registros en el archivo,
+        y retorna una lista con todos los objetos creados a partir de ellos.
+        Cada clase implementa esta operación a su manera. Si no hay registros para leer,
+        se retorna una lista vacía.}
+        class function recuperarLista(direccionCompleta:string):TListaArchivable; virtual; abstract;
     end;
 
     { TListaArchivable }
@@ -62,15 +82,23 @@ type
         atrNodo:TNodo;
         atrIterador:TNodo;
       public
+        //Crea una lista de archivables, colocando el atrNodo y atrIterador en NIL
         constructor create; overload;
+        //Elimina el objeto creado
         destructor destroy; override;
+        //Agrega un archivo a la lista
         procedure agregar(const a:TArchivable);
+        //Revisa si existe el archivo
         function existe(const a:TArchivable):boolean;
+        //elimina el archivo pasado como argumento
         procedure quitar(const a:TArchivable);
+        //Revisa si la lista es vacia
         function esVacia:boolean;
+        //Devuelve el archivo de donde esta posicionado atrIterador
         function get:TArchivable;
         //Mueve el iterador
         procedure siguiente;
+        //Revisa si hay un nodo donde pueda moverse el iterador
         function haySiguienteNodo:boolean;
         //Mueve el iterador al inicio
         procedure reset;
@@ -119,43 +147,101 @@ begin
 end;
 
 procedure TListaArchivable.agregar(const a: TArchivable);
+var novo:TNodo;
 begin
-
+  novo:= TNodo.create(a);
+  if self.esVacia then begin
+    //Aqui el nodo literalmente se agraga en como nodo principal
+    self.atrNodo:=novo;
+  end else if self.atrIterador = NIL then begin
+    //Aqui lo tenemos que agregar al inicio de la lista
+    novo.Next:=self.atrNodo;
+    self.atrNodo:=novo;
+  end else if self.atrIterador.Next<>NIL then begin
+    //Aqui el iterador esta en el medio de la lista
+    novo.Next:=self.atrIterador.Next;
+    self.atrIterador.Next:=novo;
+  end else if self.atrIterador.Next = NIL then begin
+    //Aqui el iterador esta en el final de la lista
+    self.atrIterador.Next:=novo;
+  end;
 end;
 
 function TListaArchivable.existe(const a: TArchivable): boolean;
+var apuntador:TNodo; //Lo creamos para recorrer la lista
 begin
+  result:=FALSE;
+  if self.esVacia then EXIT;
+  apuntador:=self.atrNodo;
+  while apuntador<>NIL do begin
+    if apuntador.Contenido = a then begin
+       result:=TRUE;
+       EXIT;
+    end;
+    apuntador:=apuntador.Next;
+  end;
 
 end;
 
 procedure TListaArchivable.quitar(const a: TArchivable);
+var anterior,apuntador:TNodo; //Creados para recorrer la lista
 begin
+  if not self.existe(a) then EXIT;
+  //Si existe tenemos que ubicarlo donde esta guardao
+  apuntador:=self.atrNodo;
+  anterior:=NIL;
+  while apuntador<>NIL do begin
+    if apuntador.Contenido = a then break;
+    anterior:=apuntador;
+    apuntador:=apuntador.Next;
+  end;
+  //Ahora tenemos que ubicar en parte de la lista esta
+  if anterior = NIL then begin
+     //El nodo a eliminar esta al inicio de la lista
+     self.atrNodo:=self.atrNodo.Next;
+  end else if (anterior<>NIL) and (apuntador<>NIL) then begin
+     //El nodo a eliminar esta en el medio de la lista
+     anterior.Next:=apuntador.Next;
+  end else begin
+     //Aquie el nodo esta al final de la lista
+     anterior.Next:= NIL;
+  end;
+  apuntador.destroy;
 
 end;
 
 function TListaArchivable.esVacia: boolean;
 begin
-
+  result:= (self.atrNodo = NIL);
 end;
 
 function TListaArchivable.get: TArchivable;
 begin
-
+  result:=self.atrIterador.Contenido;
 end;
 
 procedure TListaArchivable.siguiente;
 begin
-
+  if self.atrIterador = NIL then begin
+     //Aqui lo movemos al primer nodo
+     self.atrIterador:=self.atrNodo;
+  end else if self.haySiguienteNodo then begin
+     //Lo movemos en medio de la lista
+     self.atrIterador:=self.atrIterador.Next;
+  end else begin
+     //Significa que recorrimos todo la lista, entonces lo volvemos NIL el Iterador
+    self.atrIterador:=NIL;
+  end;
 end;
 
 function TListaArchivable.haySiguienteNodo: boolean;
 begin
-
+  result:=(self.atrIterador.Next <> NIL);
 end;
 
 procedure TListaArchivable.reset;
 begin
-
+  self.atrIterador:=self.atrNodo;
 end;
 
 { TListaArchivable.TNodo }
@@ -173,6 +259,7 @@ end;
 
 destructor TListaArchivable.TNodo.destroy;
 begin
+  FreeAndNil(self);
   inherited destroy;
 end;
 
@@ -203,6 +290,9 @@ TArchivable.const_NOMBRE_ARCHIVO_ESPECIES:= 'especies.db';
 TArchivable.const_NOMBRE_ARCHIVO_MASCOTAS:= 'mascotas.db';
 TArchivable.const_NOMBRE_ARCHIVO_PERSONAS:= 'personas.db';
 
+//Primero creamos el directorio si no existe
+if not DirectoryExists(TArchivable.const_NOMBRE_DIRECTORIO_DATABASE) then
+   CreateDir(TArchivable.const_NOMBRE_DIRECTORIO_DATABASE);
 
 
 end.
